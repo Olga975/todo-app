7413148ca4affec5c212dec2144edc7ce9a75ea4
Olga Adams <helga7777@gmail.com>
2023-11-07 22:01:58 -0700

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
+function useSemiPersistentState(key, initialState) {
+  const [value, setValue] = useState(() => {
+    const savedValue = JSON.parse(localStorage.getItem(key));
+    return savedValue !== null ? savedValue : initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
 function App() {
-  const [todoList, setTodoList] = useState([]); 
+  const [todoList, setTodoList] = useSemiPersistentState('savedTodoList', []);
 
   function addTodo(newTodo) {
     setTodoList((prevTodos) => [...prevTodos, newTodo]);
@@ -11,9 +24,9 @@ function App() {
 
   return (
     <>
-    <h1>Todo List</h1>
-    <AddTodoForm onAddTodo={addTodo} />
-    <TodoList todoList={todoList} /> 
+      <h1>Todo List</h1>
+      <AddTodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </>
   );
 }
